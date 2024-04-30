97695dc1d2ab338217c65e2d7f9f2d3548020291
Xernary <nicolalggiuffrida@gmail.com>
2024-04-30 13:52:33 +0200

    implemented version specification

---
+++ testt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+int main(){
+
+  printf("%d\n", atoi("aaaaa jfju88282"));
+  return 0;
+}
--- update-discord.c
+++ update-discord.c
@@ -11,6 +11,11 @@
 #define PATTERN_SIZE 12
 #define COMMAND "sudo chmod a+w "
 
+void print_help(){
+  printf("Usage: update-discord [VERSION]\n");
+  exit(1);
+}
+
 int open_file(){
 
   int fd;
@@ -27,7 +32,6 @@ int open_file(){
     memset(command, 0, command_size);
     strcpy(command, COMMAND);
     strcat(command, PATH);
-    printf("command = %s\n", command);
     system(command);
 
     if((fd = open(PATH, O_RDWR)) == -1){
@@ -39,7 +43,7 @@ int open_file(){
   return fd;
 }
 
-void increase_version(const int fd, char* const buff, char* const version){
+void update_version(const int fd, char* const buff, char* const version, int ver, char* new_version){
 
   char* const version_string = malloc(sizeof(char) * 5);
   memset(version_string, 0, 5);
@@ -54,29 +58,32 @@ void increase_version(const int fd, char* const buff, char* const version){
     if(ch == '.') dots++;
   }
 
-
   int old_version_number = atoi(version_string);
+  int new_version_number; //= ((ver == -1) ? old_version_number+1 : ver);
+  if(ver == -1) new_version_number = old_version_number+1;
+  else new_version_number = ver;
 
-  char* new_version_string;
-  sprintf(new_version_string, "%d", old_version_number+1);
+
+  char new_version_string[10];
+  memset(new_version_string, 0, 10);
+  sprintf(new_version_string, "%d", new_version_number);
 
   char* output = malloc(sizeof(char) * BUFF_SIZE);
   memset(output, 0, BUFF_SIZE);
   strncpy(output, buff, strlen(buff) - strlen(version) + end-1);
-  printf("\n%s\n", output);
   strcat(output, new_version_string);
-  printf("\n%s\n", output);
   strcat(output, &buff[strlen(buff)-1- strlen(version) + end +strlen(version_string)]);
 
   lseek(fd, 0, SEEK_SET);
   write(fd, output, strlen(output));
 
+  strcpy(new_version, new_version_string);
   free(version_string);
 
   return;
 }
 
-int main(){
+int main(int argn, char** args){
 
   int fd = open_file();
 
@@ -90,7 +97,20 @@ int main(){
     exit(1);
   }
 
-  increase_version(fd, buff, version+PATTERN_SIZE); 
+  if(argn != 1 && argn != 2){
+    print_help();
+    exit(1);
+  }
+
+  int ver = -1;
+  if(argn == 2){
+    if(args[1] == "--help") print_help();
+    else if((ver = atoi(args[1])) == 0) print_help();
+  }
+
+  char new_version[10];
+  update_version(fd, buff, version+PATTERN_SIZE, ver, new_version); 
+  printf("Updated discord to version 0.0.%s\n", new_version);
 
   close(fd);
 
